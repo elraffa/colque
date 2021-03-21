78fdb9bb7cd7ff6e65c4e6ab03dae4c7ae03ed1a
elraffa <federicoruffa@gmail.com>
2021-03-21 14:12:50 -0300

    add social buttons

--- src/components/ui/MobileNavbar.js
+++ src/components/ui/MobileNavbar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 import { FaFacebook } from "@react-icons/all-files/fa/FaFacebook"
 import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram"
 import { FaWhatsapp } from "@react-icons/all-files/fa/FaWhatsapp"
@@ -10,19 +11,23 @@ const MobileNavbar = props => {
     <div
       style={{
         backgroundColor: `#7365aa`,
-        padding: "6px 13px",
+        padding: "10px 13px",
         color: "#fff",
         display: "flex",
         justifyContent: "space-between",
         fontSize: "1.6rem",
       }}
     >
-      <div className="social">
-        <FaFacebook style={{ marginRight: "5px" }} />
-        <FaInstagram style={{ marginRight: "5px" }} />
-        <FaWhatsapp style={{ marginRight: "5px" }} />
+      <div className="social" style={{ fontSize: "2rem" }}>
+        <FaFacebook style={{ marginRight: "7px" }} />
+        <Link to="https://www.instagram.com/fidelvictorcolque/">
+          <FaInstagram style={{ marginRight: "7px" }} />
+        </Link>
+        <Link to="https://api.whatsapp.com/send?phone=541139504487">
+          <FaWhatsapp style={{ marginRight: "7px" }} />
+        </Link>
       </div>
-      <div style={{ textAlign: "right" }}>
+      <div style={{ textAlign: "right", fontSize: "2rem" }}>
         <RiMenu3Fill />
       </div>
     </div>
